9b8921b3518e2f886674043fa27f36e8c9a9035d
Shilton <shilton.sistemas@gmail.com>
2019-08-29 19:47:23 -0300

    Criando component footer

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import '../common/template/dependencias'
 import Header from '../common/template/header'
 import Sidebar from '../common/template/sidebar'
+import Footer from '../common/template/footer'
 
 export default props => (
     <div className='wrapper'>
         <Header/>
         <Sidebar/>
+        <div className='content-wrapper'>
+            <h1>Container</h1>
+        </div>
+        <Footer/>
     </div>
 )
