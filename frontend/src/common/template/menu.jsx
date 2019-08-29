9efa7fedeaedccf71e5e19d82e6f8f197c76cfdf
Shilton <shilton.sistemas@gmail.com>
2019-08-29 19:35:52 -0300

    Criando component item de menu e meu treeview

--- frontend/src/common/template/menu.jsx
+++ frontend/src/common/template/menu.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import MenuItem from './menuItem'
+import MenuTree from './menuTree'
 
 export default props => (
     <ul className='sidebar-menu'>
-        
+        <MenuItem url='#' icon='dashboard' label='Dashboard'/>
+
+        <MenuTree label='Cadastro' icon='edit'>
+            <MenuItem url='#/cadastros' label='Transações' icon='usd'/> 
+        </MenuTree>
     </ul>
     
 )
