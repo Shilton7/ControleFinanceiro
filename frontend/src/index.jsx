b25e24c05fbcc57146aec0e1f62de4b9da382685
Shilton <shilton.sistemas@gmail.com>
2019-08-30 12:35:29 -0300

    Habilitando devTools  no console

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -7,7 +7,11 @@ import { Provider } from 'react-redux'
 import reducers from './main/reducers'
 import promise from 'redux-promise'
 
-const store = applyMiddleware(promise)(createStore)(reducers)
+// Habilitando devTools console
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ &&
+window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store = applyMiddleware(promise)(createStore)(reducers, devTools)
 
 ReactDOM.render(
     <Provider store={store}>
