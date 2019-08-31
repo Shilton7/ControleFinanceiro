a45fe001ce3778fbe283cb03e3d8b75778cdde09
Shilton <shilton.sistemas@gmail.com>
2019-08-31 13:14:15 -0300

    Importando o Redux Form

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
 import TransacaoReducer from '../transacao/transacaoReducer'
@@ -6,7 +7,8 @@ import TransacaoReducer from '../transacao/transacaoReducer'
 const rootReducer = combineReducers ({
     dashboard: DashboardReducer,
     tab: TabReducer,
-    transacao: TransacaoReducer
+    transacao: TransacaoReducer,
+    form: formReducer
 })
 
 export default rootReducer
