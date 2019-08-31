09c6deb3baba847bfc1004d87b34c551007e336a
Shilton <shilton.sistemas@gmail.com>
2019-08-31 14:14:20 -0300

    Atualizando lista após salvar

--- frontend/src/transacao/transacaoActions.js
+++ frontend/src/transacao/transacaoActions.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import { toastr } from 'react-redux-toastr'
+import { reset as resetForm } from 'redux-form'
+import { showTabs, selectTab } from '../common/tab/tabActions'
 
 const base_url = 'http://localhost:3003/api/v1'
 
@@ -12,15 +14,23 @@ export function listaTransacao() {
 }
 
 export function criar(valores) {
-    axios.post(`${base_url}/transacao`, valores)
-        .then(res => {
-            toastr.success('Sucesso', 'Salvo com sucesso!')
-        })
-        .catch(e => {
-            e.response.data.errors.forEach(error => toastr.error('Erro', error ))
-        })
+    return dispatch => {
+        axios.post(`${base_url}/transacao`, valores)
+            .then(res => {
+                toastr.success('Sucesso', 'Salvo com sucesso!')
+                dispatch([
+                    resetForm('TransacaoForm'),
+                    listaTransacao(),
+                    selectTab('tabListar'),
+                    showTabs('tabListar','tabCreate')
+
+                ])
+            })
+            .catch(e => {
+                e.response.data.errors.forEach(error => toastr.error('Erro', error))
+            })
+
+    }
+    
         
-    return {
-        type: 'salvar_transacao'
-        }
 }
